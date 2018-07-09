06fafa296b196eb626b944e5e6dad83a9f5517b1
Lyy <yiyanluo0417@gmail.com>
2018-07-09 14:27:56 -0400

    updates on people and contact page

--- cssjs/jscode.js
+++ cssjs/jscode.js
@@ -27,3 +27,11 @@ function autoSlide() {
 
   setTimeout(autoSlide, 5000);
 }
+
+function myMap() {
+  var mackenzie = {lat: 45.38488, lng: -75.7002047};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12, center: mackenzie
+  });
+  var marker = new google.maps.Marker({position: mackenzie, map: map});
+}
